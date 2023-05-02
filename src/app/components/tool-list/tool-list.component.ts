428ba0f571373a55fef4c8df89ecbea92016b355
David Sequera <davidsequerads@gmail.com>
2023-05-01 19:20:28 -0500

    solved searchbar

--- src/app/components/tool-list/tool-list.component.ts
+++ src/app/components/tool-list/tool-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Apollo } from 'apollo-angular';
+import { Subscription } from 'rxjs';
 import { GET_ALL_TOOLS, GET_TOOL_BY_NAME } from 'src/app/graphql/graphql.tools.queries';
 import { UserContextService } from 'src/app/services/context/user-context.service';
 
@@ -11,7 +12,7 @@ import { UserContextService } from 'src/app/services/context/user-context.servic
 /**
  * Componente que muestra la lista de herramientas
  */
-export class ToolListComponent implements OnInit {
+export class ToolListComponent implements OnInit, OnDestroy {
   tools?: any[];
   brands?: any[];
   loading = true;
@@ -19,7 +20,9 @@ export class ToolListComponent implements OnInit {
 
   filterOn = false;
 
+  
   error: any;
+  searchRequestSubscriptions: Subscription[] = [];
   /**
    * Servicio de contexto de usuario
    */
@@ -35,6 +38,12 @@ export class ToolListComponent implements OnInit {
 
   ngOnInit(): void {
     this.addTools();
+    this.tools;
+
+  }
+
+  ngOnDestroy(): void {
+      
   }
 
   openFilter() {
@@ -42,23 +51,28 @@ export class ToolListComponent implements OnInit {
   }
 
   addTools() {
+
     this.apollo.use('tools').watchQuery({ // Realiza la consulta de autenticación
       query: GET_ALL_TOOLS,
     })
     .valueChanges
-    .subscribe({ // Suscribe a los resultados de la consulta
-      next: ({ data }: any ) => { // Si la consulta es exitosa
-        console.log(data);
-        this.tools = data?.getAllTools;
-        this.brands = data?.getBrands;
-        this.loading = data.loading;
-        this.error = data.error;
-      },
-      error: (error) => { // Si la consulta falla
-        console.error('There was an error sending the query', error);
-        this.error = error;
-      }
-    })
+    .pipe(
+      // debounceTime(1000),
+      // distinctUntilChanged(),
+      // switchMap(({data}: any) => data),
+    ).subscribe({ // Suscribe a los resultados de la consulta
+          next: ({data}: any ) => { // Si la consulta es exitosa
+            console.log(data);
+            this.tools = data?.getAllTools;
+            this.brands = data.getBrands;
+            this.loading = data.loading;
+            this.error = data.error;
+          },
+          error: (error) => { // Si la consulta falla
+            console.error('There was an error sending the query', error);
+            this.loading = false;
+            this.error = error;
+          }})
 
   }
 
@@ -68,32 +82,34 @@ export class ToolListComponent implements OnInit {
     if(Object.values(this.userContext.brandsChecked).includes(true)) {
       return this.userContext.brandsChecked[tool.brand.id];
     }
-    console.log("[BRANDS]",this.userContext.brandsChecked);
     return true;
-    //  check if the tool is from a brand selected
   }
 
 
-  onSeachbarInput(name: string) {
+  cancelPendingRequests() {
+    this.searchRequestSubscriptions.forEach(sub => sub.unsubscribe());
+  }
+  onSeachbarChange(name: string) {
     this.search = name;
-    console.log("[NAME]",name);
-    if(name === '') {
+    this.cancelPendingRequests();
+    console.log("[SearchTerm]",this.search);
+    if(this.search === '') {
       this.addTools();
       return;
     }
-    this.apollo.use('tools').watchQuery({ // Realiza la consulta de autenticación})
+    const apiSubscription = this.apollo.use('tools').watchQuery({ // Realiza la consulta de autenticación})
       query: GET_TOOL_BY_NAME,
       variables: {
         "toolPageInput": {
             "page": 2,
             "size": 20
         },
-        "search": name 
+        "search": this.search 
       }
     })
     .valueChanges
     .subscribe({ // Suscribe a los resultados de la consulta
-      next: ({ data }: any ) => { // Si la consulta es exitosa
+      next: ( {data}: any ) => { // Si la consulta es exitosa
         console.log(data);
         this.tools = data?.getToolsByName;
         this.loading = data.loading;
@@ -101,8 +117,11 @@ export class ToolListComponent implements OnInit {
       },
       error: (error) => { // Si la consulta falla
         console.error('There was an error sending the query', error);
+        this.loading = false;
         this.error = error;
       }})
+
+    this.searchRequestSubscriptions.push(apiSubscription);
   }
 
 
